5e92e7f81a26e353f2e7ae1f5b1c5f961dbe0e29
cjapp <cjapp@mail.csuchico.edu>
2019-12-01 15:08:29 -0800

    Added a range function to random

--- src/hearth/random.cpp
+++ src/hearth/random.cpp
@@ -1,25 +1,46 @@
+/**
+ * @file random.cpp
+ * @brief The Random class definition file for the hearth engine.
+ * @author Colby J
+ * @version 0.0.1
+ * @date 2019-12-01
+ * @edited 2019-12-01
+ */
+
 #include "random.h"
 
-#include<cstdlib>
-#include<time.h>
-#include<iostream>
+#include<cstdlib>//srand() and rand()
+#include<time.h>//time()
+#include<iostream>//std::cerr and std::endl
 
 namespace Hearth
 {
+  //Determines whether or not the random singleton has been seeded
   bool Random::m_seeded = false;
 
 
+  /**
+   * @brief Random class constructor, is usually never needed to be called
+   */
   Random::Random()
   {
     if(!m_seeded) 
       seed(time(NULL));
   }
 
+  /**
+   * @brief Random class deconstructor, is usually never needed to be called
+   */
   Random::~Random()
   {
 
   }
 
+  /**
+   * @brief Function that seeds the random number generator
+   *
+   * @param s the seed to use for the random number generator
+   */
   void Random::seed(const int s)
   {
     std::cerr << "Seeding the random number generator..." << std::endl;
@@ -27,12 +48,39 @@ namespace Hearth
     m_seeded = true;
   }
 
+  /**
+   * @brief function that will return a random int
+   *
+   * @return a random int value
+   */
   int Random::random() 
   {
     if(!m_seeded)
       seed(time(NULL));
 
     return rand();
+  }
+
+  /**
+   * @brief This function will generate a random int within the range provided
+   *
+   * @param min the minimum possible value the generator can generate
+   * @param max the maximum possible value the generator can generate
+   *
+   * @return a random number within range provided in function call
+   */
+  int Random::random(int min, int max)
+  {
+    //if min and max are the same return max
+    if min == max ? return max;
+ 
+    if(min > max)
+    {
+      std::cerr << "ERROR: Random::random()- Minimum is greater than maximum value." << std::endl;
+      return 0;
+    }
+
+    return random()%(max + 1 - min) + min;
   }
 
 
--- src/hearth/random.h
+++ src/hearth/random.h
@@ -1,23 +1,32 @@
+/**
+ * @file random.h
+ * @brief The Random class header file for the Hearth Engine.
+ * @author Colby J
+ * @version 0.0.1
+ * @date 2019-12-01
+ * @edited 2019-12-01
+ */
 #ifndef RANDOM_H
 #define RANDOM_H 
 
 namespace Hearth
 {
 
-class Random
-{
-  private:
-    static bool m_seeded;
+  class Random
+  {
+    private:
+      static bool m_seeded;
 
-  public:
-    Random();
-    ~Random();
+    public:
+      Random();
+      ~Random();
 
-    static void seed(const int s);
-    static int random();
-    
+      static void seed(const int s);
 
-};
+      static int random();
+      static int random(int min, int max);
+
+  };
 
 }//end Hearth
 
